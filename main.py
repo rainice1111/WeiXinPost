574ccc49618888538bf93401f9415d88550c8188
rainice1111 <159868798+rainice1111@users.noreply.github.com>
2024-12-03 11:38:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -61,29 +61,28 @@ def get_Today_Week():
 
 
 # 获取本周课程
-def get_Week_Classes(w):
-    if w is not None:
-        week_Class = config.classes.get(w)
-    else:
-        week = get_Today_Week()
-        week_Class = config.classes.get(week)
-    return week_Class
+#def get_Week_Classes(w):
+    #if w is not None:
+     #   week_Class = config.classes.get(w)
+#    else:
+ #       week = get_Today_Week()
+#        week_Class = config.classes.get(week)
+#    return week_Class
 
 
-# 获取今日课程
-def get_Today_Class():
-    year = localtime().tm_year
-    month = localtime().tm_mon
-    day = localtime().tm_mday
-    today = datetime.date(datetime(year=year, month=month, day=day))
-    todayClasses = get_Week_Classes(None)[today.weekday()]
-    return todayClasses
+# 获取今日课程def get_Today_Class():
+    #year = localtime().tm_year
+    #month = localtime().tm_mon
+    ##day = localtime().tm_mday
+    #today = datetime.date(datetime(year=year, month=month, day=day))
+    #todayClasses = get_Week_Classes(None)[today.weekday()]
+    #return todayClasses
 
 
 # 获取指定星期几的课程
-def get_Class(day):
-    theClasses = get_Week_Classes(None)[day]
-    return theClasses
+#def get_Class(day):
+   # theClasses = get_Week_Classes(None)[day]
+    #return theClasses
 
 
 # # 发送本周所有课程，周一的时候发
@@ -131,208 +130,19 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
     # 获取在一起的日期差
     love_days = str(today.__sub__(love_date)).split(" ")[0]
     # 获取生日的月和日
-    birthday_month = int(config.birthday.split("-")[1])
-    birthday_day = int(config.birthday.split("-")[2])
+    #birthday_month = int(config.birthday.split("-")[1])
+    #birthday_day = int(config.birthday.split("-")[2])
     # 今年生日
-    year_date = date(year, birthday_month, birthday_day)
+    #year_date = date(year, birthday_month, birthday_day)
     # 计算生日年份，如果还没过，按当年减，如果过了需要+1
-    if today > year_date:
-        birth_date = date((year + 1), birthday_month, birthday_day)
-        birth_day = str(birth_date.__sub__(today)).split(" ")[0]
-    elif today == year_date:
-        birth_day = 0
-    else:
-        birth_date = year_date
-        birth_day = str(birth_date.__sub__(today)).split(" ")[0]
-
-    theClass = get_Today_Class()
-    theuser = to_user[0]
-    data = {
-        "touser": theuser,
-        "template_id": config.template_id1,
-        "url": "http://weixin.qq.com/download",
-        "topcolor": "#FF0000",
-        "data": {
-            "weeks": {
-                "value": weeks,
-                "color": "#00FFFF"
-            },
-            "date": {
-                "value": "{} {}".format(today, week),
-                "color": "#00FFFF"
-            },
-            "city": {
-                "value": city_name,
-                "color": "#808A87"
-            },
-            "weather": {
-                "value": weather,
-                "color": "#ED9121"
-            },
-            "min_temperature": {
-                "value": min_temperature,
-                "color": "#00FF00"
-            },
-            "max_temperature": {
-                "value": max_temperature,
-                "color": "#FF6100"
-            },
-            "love_day": {
-                "value": love_days,
-                "color": "#87CEEB"
-            },
-            "birthday": {
-                "value": birth_day,
-                "color": "#FF8000"
-            },
-            "firstClass": {
-                "value": theClass[0],
-                "color": "#FF8000"
-            },
-            "secondClass": {
-                "value": theClass[1],
-                "color": "#FF8000"
-            },
-            "thirdClass": {
-                "value": theClass[2],
-                "color": "#FF8000"
-            },
-            "fourthClass": {
-                "value": theClass[3],
-                "color": "#FF8000"
-            },
-            "fifthClass": {
-                "value": theClass[4],
-                "color": "#FF8000"
-            },
-            "sixthClass": {
-                "value": theClass[5],
-                "color": "#FF8000"
-            }
-        }
-    }
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-    }
-    response = post(url, headers=headers, json=data)
-    print(response.text)
-
-
-# 发送课程消息
-def send_Class_Message(to_user, access_token, classInfo):
-    url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
-    theuser = to_user[0]
-    data = {
-        "touser": theuser,
-        "template_id": config.template_id2,
-        "url": "http://weixin.qq.com/download",
-        "topcolor": "#FF0000",
-        "data": {
-            "classInfo": {
-                "value": classInfo,
-                "color": "#FF8000"
-            }
-        }
-    }
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-    }
-    response = post(url, headers=headers, json=data)
-    print(response.text)
-
-
-# 发送晚安心语及第二天课程
-def send_Good_Night(to_user, access_token):
-    week_list = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-    }
-    # 获取天行数据晚安心语
-    txUrl = "http://api.tianapi.com/wanan/index"
-    key = config.good_Night_Key
-    pre_data = {"key": key}
-    # param = json.dumps((pre_data))
-    r = post(txUrl, params=pre_data, headers=headers)
-    print("r:", r.text)
-    good_Night = r.json()["newslist"][0]["content"]
-    # good_Night = "晚安"
-    # 获取第二天课表
-    year = localtime().tm_year
-    month = localtime().tm_mon
-    day = localtime().tm_mday
-    today = datetime.date(datetime(year=year, month=month, day=day))
-    weekClasses = get_Week_Classes(None)
-    week = week_list[(today.weekday() + 1) % 7]
-    theClass = []
-    if (today.weekday() + 1) % 7 == 0:
-        weekClasses = get_Week_Classes(get_Today_Week())
-        theClass = weekClasses[0]
-    else:
-        theClass = weekClasses[today.weekday() + 1]
-
-    url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
-    theuser = to_user[0]
-    data = {
-        "touser": theuser,
-        "template_id": config.template_id3,
-        "url": "http://weixin.qq.com/download",
-        "topcolor": "#FF0000",
-        "data": {
-            "goodNight": {
-                "value": good_Night,
-                "color": "#87CEEB"
-            },
-            "week": {
-                "value": week,
-                "color": "#00FFFF"
-            },
-            "firstClass": {
-                "value": theClass[0],
-                "color": "#FF8000"
-            },
-            "secondClass": {
-                "value": theClass[1],
-                "color": "#FF8000"
-            },
-            "thirdClass": {
-                "value": theClass[2],
-                "color": "#FF8000"
-            },
-            "fourthClass": {
-                "value": theClass[3],
-                "color": "#FF8000"
-            },
-            "fifthClass": {
-                "value": theClass[4],
-                "color": "#FF8000"
-            },
-            "sixthClass": {
-                "value": theClass[5],
-                "color": "#FF8000"
-            }
-        }
-    }
-    response = post(url, headers=headers, json=data)
-    print(response.text)
-
-
-# 计算时间间隔
-def calculate_Time_Difference(t1, t2):
-    h1 = int(t1[0:2])
-    h2 = int(t2[0:2])
-    m1 = int(t1[3:5])
-    m2 = int(t2[3:5])
-    s1 = int(t1[6:8])
-    s2 = int(t2[6:8])
-    d1 = datetime(2022, 1, 1, h1, m1, s1)
-    d2 = datetime(2022, 1, 1, h2, m2, s2)
-    return (d1 - d2).seconds
+    #if today > year_date:
+    #    birth_date = date((year + 1), birthday_month, birthday_day)
+    #    birth_day = str(birth_date.__sub__(today)).split(" ")[0]
+    #elif today == year_date:
+     #   birth_day = 0
+    #else:
+      #  birth_date = year_date
+      #  birth_day = str(birth_date.__sub__(today)).split(" ")[0]
 
 
 if __name__ == '__main__':
@@ -350,50 +160,4 @@ if __name__ == '__main__':
     if datetime.now().strftime('%H:%M:%S') < config.post_Time:
         send_message(user, accessToken, city, weather, max_temperature, min_temperature)
         isPost = True
-    # 课程提醒推送
-    todayClasses = get_Today_Class()
-    time_table = config.time_table
-    for i in range(len(time_table)):
-        if isPost:
-            break
-        reminderTime = time_table[i]
-        while True:
-            nowTime = datetime.now().strftime('%H:%M:%S')
-            print("当前时间:", nowTime)
-            if reminderTime == nowTime:
-                if len(todayClasses[i]) != 0:
-                    classInfo = "课程信息: " + todayClasses[i] + "\n" + "上课时间: " + config.course_Time[i] + "\n"
-                    print(classInfo)
-                    send_Class_Message(user, accessToken, classInfo)
-                    print("课程信息推送成功！")
-                isPost = True
-                break
-            elif reminderTime < nowTime:
-                break
-            # 通过睡眠定时
-            defference = calculate_Time_Difference(reminderTime, nowTime) - 3
-            print("课程推送时间差：", defference, "秒")
-            if defference > 0:
-                print("开始睡眠: 等待推送第", i + 1, "讲课")
-                time.sleep(defference)
-                print("结束睡眠")
-    while True:
-        goodNightTime = config.good_Night_Time
-        nowTime = datetime.now().strftime('%H:%M:%S')
-        if goodNightTime == nowTime:
-            # 发送晚安心语
-            send_Good_Night(user, accessToken)
-            print("晚安心语推送成功！")
-            break
-        elif goodNightTime < nowTime:
-            print("当前时间已过晚安心语推送设置的时间！")
-            break
-        elif calculate_Time_Difference(goodNightTime, nowTime) > 120:
-            break
-        # 通过睡眠定时
-        defference = calculate_Time_Difference(goodNightTime, nowTime) - 3
-        print("晚安心语推送时间差：", defference, "秒")
-        if defference > 0:
-            print("开始睡眠:等待推送晚安心语")
-            time.sleep(defference)
-            print("结束睡眠")
+    
